f1215be734963ecfa55d2920d6a25d7abf20c89a
axha <axha@averna.com>
2023-12-18 09:50:53 -0500

    wip

--- src/utils/config-utils.ts
+++ src/utils/config-utils.ts
@@ -1,6 +1,5 @@
 import { BASE_CONFIG } from "../../astro.config.mjs";
 
 export function withBase(url: string): string {
-  console.log("base config", BASE_CONFIG);
   return BASE_CONFIG ? `${BASE_CONFIG}${url}` : url;
 }
