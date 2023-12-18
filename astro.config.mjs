e7ad69dca3e29213cace6bb1fea03d6ec874810b
axha <axha@averna.com>
2023-12-18 09:15:48 -0500

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
-const SITE_URL = "https://axhatem.github.io/lpxtest";
+const SITE_CONFIG = "https://axhatem.github.io/";
+const BASE_CONFIG = "/lpxtest";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: SITE_URL
+  site: SITE_CONFIG,
+  base: BASE_CONFIG,
 });
