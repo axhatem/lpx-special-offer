ee93842ab697488230bcd1ad51638c3c906e6f81
Axel Ha <axelensiie@gmail.com>
2023-12-28 14:29:28 -0500

    test lower font

--- src/utils/config-utils.ts
+++ src/utils/config-utils.ts
@@ -1,7 +1,7 @@
 const IS_DEV = process.env.NODE_ENV === "development";
 
 export const SITE_CONFIG = "https://axhatem.github.io";
-export const BASE_CONFIG = IS_DEV ? "" : "/lpxtest";
+export const BASE_CONFIG = IS_DEV ? "" : "/lpx-special-offer";
 
 export function withBase(url: string): string {
   return BASE_CONFIG ? `${BASE_CONFIG}${url}` : url;
